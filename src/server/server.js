9d317d453db9c918207032ea532d70800c530484
Jared <weissjmw@gmail.com>
2023-05-29 20:59:26 +0300

    imported pg and dotenv, didnt test yet

--- src/server/server.js
+++ src/server/server.js
@@ -1,5 +1,31 @@
 import http from "http";
 
+import { Client } from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const connectDb = async () => {
+  try {
+    const client = new Client({
+      user: process.env.PGUSER,
+      host: process.env.PGHOST,
+      database: process.env.PGDATABASE,
+      password: process.env.PGPASSWORD,
+      port: process.env.PGPORT,
+    });
+
+    await client.connect();
+    const res = await client.query("SELECT * FROM some_table");
+    console.log(res);
+    await client.end();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+connectDb();
+
 const hostname = "127.0.0.1";
 const port = 5000;
 
